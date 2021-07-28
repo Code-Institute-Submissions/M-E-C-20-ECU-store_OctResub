eafd4c0b3c9bbbb4ceebf6a04a029db586261474
M-E-C-20 <mcasey8511@gmail.com>
2021-07-28 01:27:00 +0000

    Customise products admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'manufacturer',
+        'model',
+        'year',
+        'version',
+        'enginecode',
+        'fuel',
+        'kw',
+        'ecubrand',
+        'ecuversion',
+        'price',
+        'sku',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Product, ProductAdmin)
