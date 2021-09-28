7dd5e4a3d7f40719854e0f280919fc2312816e06
M-E-C-20 <mcasey8511@gmail.com>
2021-09-28 01:32:39 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JMbSIHiG39TPqklf63q3bbuh3Dl2nN52Q4ilbHPfzCFTahzD6mgIFv8OC21yHW60BiMgkcOJ0Af94eTZzDQmTvb00GAfkuTJe',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
