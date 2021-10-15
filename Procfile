f6254988aaaeda794a6a375e9cf9d48403d46776
M-E-C-20 <mcasey8511@gmail.com>
2021-10-15 02:49:58 +0000

    Deploy app to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ecu_store.wsgi:application
--- ecu_store/settings.py
+++ ecu_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r50da7)uk3hwum%k&o5@feh$_s00f$6)cm(3i8=ytip86+61zb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecu-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'ecu_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
