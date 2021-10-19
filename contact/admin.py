3fb485353a2cc11ca5da0b895488c201b1a7429a
M-E-C-20 <mcasey8511@gmail.com>
2021-10-19 00:33:46 +0000

    Customised admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'subject',
+    )
+
+admin.site.register(Contact, ContactAdmin)
