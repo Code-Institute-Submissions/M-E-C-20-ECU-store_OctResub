ef39e2c3132b966a225634258b1ac4f5ccd317f8
M-E-C-20 <mcasey8511@gmail.com>
2021-10-20 06:05:59 +0000

    Added contact message detail functionality

--- contact/urls.py
+++ contact/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.contact_view, name='contact'),
     path('admin/', views.contact_admin, name='contact_admin'),
+    path('admin/contact_message/<contact_id>',
+         views.contact_message, name='contact_message'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import Contact
 
@@ -6,9 +6,9 @@ from .forms import ContactForm
 
 
 def contact_view(request):
+    """ A view to handle the contact form """
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        posts = Contact.objects.all()
         if form.is_valid():
             form.save()
             messages.success(request, 'Thank you for your enquiry. A member of our team will be in touch soon!')
@@ -24,10 +24,23 @@ def contact_view(request):
 def contact_admin(request):
     """ A view to show all contact requests, including sorting queries """
 
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().order_by('-created')
 
     context = {
         'contacts': contacts,
     }
 
     return render(request, 'contact/contact_admin.html', context)
+
+
+def contact_message(request, contact_id):
+    """ A view to show an individual contact message """
+
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    context = {
+        'contact': contact,
+    }
+    template = 'contact/contact_message.html'
+
+    return render(request, template, context)
