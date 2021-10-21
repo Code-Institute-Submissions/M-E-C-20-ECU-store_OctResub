c464402d940223966c70f24c9fb62b21e45c4bd9
M-E-C-20 <mcasey8511@gmail.com>
2021-10-21 03:43:19 +0000

    Update contact model, add contact and admin tool links to the navbar

--- contact/models.py
+++ contact/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=255, null=False, blank=False)
+    message = models.TextField(max_length=2000, null=False, blank=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
