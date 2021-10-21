077a0a297db38aa276cc5b7a6f7e5ed9b9d00f00
M-E-C-20 <mcasey8511@gmail.com>
2021-10-21 16:52:17 +0000

    Change subject field to a dropdown select

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Contact(models.Model):
+    CONTACT_SUBJECT = [
+        ('PQ', 'Product Query'),
+        ('DL', 'Deliveries'),
+        ('RR', 'Repair Request'),
+        ('CM', 'Complaint'),
+        ('OR', 'Other...')
+    ]
+
     email = models.EmailField(max_length=254, null=False, blank=False)
-    subject = models.CharField(max_length=255, null=False, blank=False)
+    subject = models.CharField(max_length=2, choices=CONTACT_SUBJECT)
     message = models.TextField(max_length=2000, null=False, blank=False)
     created = models.DateTimeField(auto_now_add=True)
 
